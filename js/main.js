14702b6d5c33e0a07cb1041dedad580ddcb4abf9
muhammedrishadpe <rishadpe@gmail.com>
2021-07-31 12:30:11 +0530

    Get matches to current text input

--- js/main.js
+++ js/main.js
@@ -6,7 +6,16 @@ const searchStates = async searchText => {
     const res = await fetch('../data/states.json');
     const states = await res.json();
 
-    console.log(states);
-}
+    //Get matches to current text input
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+    }
+    console.log(matches);
+};
 
 search.addEventListener('input', () => searchStates(search.value));
